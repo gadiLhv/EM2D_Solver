1b7f9a14855ece32a701a4f144318c02ce4151a8
gadiLhv <gadi.lhv@gmail.com>
2017-11-04 19:18:03 +0200

    Build K-mat from materials 04112017
    1. Continued debugging 'createKmatBvect_materials' function.
    2. Added sample data for matrix build 'solver_predata'
    3. Initial test script for material K matrix.

--- Solver_2D/cem2D_createKmatBvect_materials.m
+++ Solver_2D/cem2D_createKmatBvect_materials.m
@@ -1,4 +1,4 @@
-function [K,b] = cem2D_createKmat_TM(meshData,materialList,materialAssign,simProps,f0)
+function [K,b] = cem2D_createKmatBvect_materials(meshData,materialList,materialAssign,simProps)
 % Creates an initial matrix batch using only dielectric and ferrimagnetic
 % properties of the medium. This formulation is relevant for TM(Z) polarization
 %
@@ -14,6 +14,12 @@ m0 = physical_constant('mag. constant');
 
 edgeCouples = meshData.etri;
 
+nVerts = size(meshData.verts,1);
+
+% Initial FEM matrix and source vector
+K = zeros([1 1]*nVerts);
+b = zeros([nVerts 1]);
+
 % Build matrix face by face
 for faceIdx = 1:numel(meshData.face)
   % Extract current parts material properties
@@ -24,21 +30,44 @@ for faceIdx = 1:numel(meshData.face)
   er = cMaterialProps.er;
   
   % Recover all triangles relevant to current face
-  triIdxs = meshData.tnum == faceIdx;
+  triBinMap = meshData.tnum == faceIdx;
   % Store all triplets 
-  triTriplets = meshData.tria(triIdxs,:);
+  triTriplets = meshData.tria(triBinMap,:);
   
   % Recover all edge nodes corresponding to the
   % extracted triangles:
   % 1. Determine if either of the edge nodes is present in a triangle
+  
   edgeNode1map = bsxfun(@eq,edgeCouples(:,1),permute(triTriplets,[3 1 2]));
   edgeNode2map = bsxfun(@eq,edgeCouples(:,2),permute(triTriplets,[3 1 2]));
+  
   % 2. Determine if both of them are present in a triangle
-  areBothNodesIn = (sum(edgeNode1map,3) + sum(edgeNode2map,3)) == 2;
+  areBothNodesIn = max(edgeNode1map,[],3) & max(edgeNode2map,[],3);
   % 3. Extract only nodes that consist an edge in this face.
+  [edgesInFace,~] = find(areBothNodesIn);
+  edgePairs = meshData.etri(edgesInFace,:);
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Debug: paint triangles and faces %
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  figure;
+%  patch('faces',triTriplets,...
+%        'vertices',meshData.vert, ...
+%        'facecolor',[1.,1.,1.], ...
+%        'edgecolor',[0,0,0]) ;
+%  hold on; 
+%  axis image off;
+%  edgesX = [meshData.vert(edgePairs(:,1),1) meshData.vert(edgePairs(:,2),1)].';
+%  edgesY = [meshData.vert(edgePairs(:,1),2) meshData.vert(edgePairs(:,2),2)].';
+%  plot(edgesX,edgesY,'-','linewidth',3);
+%  hold off;
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Debug: paint triangles and faces %
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
   
 end
+
 
 
 end
---
+++ test_matrix_construction_material.m
@@ -0,0 +1,36 @@
+% test_matrix_construction_material
+
+clc
+clear
+close all
+
+%% Load relevant data
+
+modelerPath = './Modeler_2D';
+mesherPath = '/home/gadi/Repositories/mesh2d';
+meshWrapperPath = './Mesh_2D';
+solverPath = './Solver_2D';
+
+addpath(modelerPath);
+addpath(mesherPath);
+addpath(meshWrapperPath);
+addpath(solverPath);
+
+load solver_predata
+
+%% Construct matrix for volume\material B.Cs.
+
+[K,b] = cem2D_createKmatBvect_materials(...
+          smoothMesh,...            % Mesh data structure
+          materialList,...          % Cell array of all materials
+          materialAssignment,...    % Cell array assigning materials to faces
+          simProps);                % Simulation properties
+
+
+
+%%
+
+%rmpath(modelerPath);
+%rmpath(mesherPath);
+%rmpath(meshWrapperPath);
+%rmpath(solverPath);
